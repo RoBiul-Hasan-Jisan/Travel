c14c5e89f6574fbd84780a642ccc620e1c7a0ad4
Robiul Hasan Jisan <robiulhasanjisan88@gmail.com>
2025-10-17 19:09:01 +0600

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,17 +6,15 @@ import joblib
 import os
 import gdown
 
-
-MODEL_FILE_ID = "1d5Gd90mf9pvm7jboPJo21BufmQ7QxOYc"          
-FEATURE_ENCODERS_FILE_ID = "1bw3JYpiXHxVsRwloBkEWEjVTrTzxf6iS" 
-TARGET_ENCODER_FILE_ID = "17_AM7STv3Tnz1uAAaelY71b6iDGxMfpP"    
+MODEL_FILE_ID = "1d5Gd90mf9pvm7jboPJo21BufmQ7QxOYc"
+FEATURE_ENCODERS_FILE_ID = "1bw3JYpiXHxVsRwloBkEWEjVTrTzxf6iS"
+TARGET_ENCODER_FILE_ID = "17_AM7STv3Tnz1uAAaelY71b6iDGxMfpP"
 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MODEL_PATH = os.path.join(BASE_DIR, "..", "travel_rf_model.joblib")
-FEATURE_ENCODERS_PATH = os.path.join(BASE_DIR, "..", "travel_label_encoders.joblib")
-TARGET_ENCODER_PATH = os.path.join(BASE_DIR, "..", "travel_target_encoder.joblib")
-
+MODEL_PATH = os.path.join(BASE_DIR, "travel_rf_model.joblib")
+FEATURE_ENCODERS_PATH = os.path.join(BASE_DIR, "travel_label_encoders.joblib")
+TARGET_ENCODER_PATH = os.path.join(BASE_DIR, "travel_target_encoder.joblib")
 
 
 def download_from_drive(file_id: str, dest_path: str):
@@ -28,11 +26,9 @@ def download_from_drive(file_id: str, dest_path: str):
         print(f" {os.path.basename(dest_path)} already exists.")
 
 
-
 download_from_drive(MODEL_FILE_ID, MODEL_PATH)
 download_from_drive(FEATURE_ENCODERS_FILE_ID, FEATURE_ENCODERS_PATH)
 download_from_drive(TARGET_ENCODER_FILE_ID, TARGET_ENCODER_PATH)
-
 
 
 def load_model():
@@ -49,7 +45,6 @@ feature_encoders = load_feature_encoders()
 target_encoder = load_target_encoder()
 
 
-
 app = FastAPI(title="Travel Destination Recommender")
 
 app.add_middleware(
@@ -61,7 +56,6 @@ app.add_middleware(
 )
 
 
-
 class TravelInput(BaseModel):
     Budget: str
     Climate: str
@@ -70,11 +64,12 @@ class TravelInput(BaseModel):
     Season: str
     Region: str
 
-
+# -----------------------------
+# Routes
+# -----------------------------
 @app.get("/")
 def root():
     return {"message": "Travel Destination Recommender API is running!"}
-
 
 @app.post("/predict")
 def predict_destination(data: TravelInput):
@@ -86,9 +81,9 @@ def predict_destination(data: TravelInput):
             try:
                 df[col] = le.transform(df[col])
             except ValueError:
-                df[col] = -1
+                df[col] = -1  # Assign -1 for unseen values
 
-    
+    # Make prediction
     pred_encoded = model.predict(df)[0]
     destination = target_encoder.inverse_transform([pred_encoded])[0]
 
